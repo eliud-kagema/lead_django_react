052dbae527c60a9cfc5613aa7a4f74dd3128cefe
Eliud Kagema <eliud.k.maina@gmail.com>
2020-08-07 16:15:10 +0300

    setting up urls for leads app

--- leads/urls.py
+++ leads/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('leads.urls')),
+    path('api/lead/', views.LeadListCreate.as_view() ),
 ]
